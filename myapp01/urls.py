f71f9df365c3f5b4ffe6cae2650732fc2ce1d0cd
unknown <bhattacharjeesumon93@gmail.com>
2020-07-24 19:26:23 +0530

    second commit

---
+++ myapp01/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from myapp01 import views
+
+app_name = "myapp01"
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('home',views.home,name='home'),
+    path('fact/<n>',views.fact,name='name'),
+]
--- p1/urls.py
+++ p1/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myapp01 import views
+from django.urls import path,include
+import myapp01 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
-    path('home/',views.home,name='home'),
-    path('fact/<n>/',views.fact,name='fact'),
+    path('myapp01/',include("myapp01.urls")),
 ]
